d5238ba63d8676684b86699cbedddbf8812ba8fd
Neil Ye <neil.ye@cloud-interactive.com>
2021-02-05 12:03:51 +0800

    Rooms

--- lib/screens/home_screens.dart
+++ lib/screens/home_screens.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/circle_button.dart';
 import 'package:flutter_facebook_responsive_ui/widgets/create_post_container.dart';
+import 'package:flutter_facebook_responsive_ui/widgets/rooms.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 import '../config/palette.dart';
@@ -40,7 +41,13 @@ class HomeScreen extends StatelessWidget {
             ],
           ),
           SliverToBoxAdapter(
-            child: CreatePostContainer(currentUser:currentUser),
+            child: CreatePostContainer(currentUser: currentUser),
+          ),
+          SliverPadding(
+            padding: const EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 5.0),
+            sliver: SliverToBoxAdapter(
+              child: Rooms(onlineUsers: onlineUsers),
+            ),
           ),
         ],
       ),
--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,2 +1,4 @@
 export 'widgets.dart';
 export 'create_post_container.dart';
+export 'rooms.dart';
+export 'profile_avatar.dart';
